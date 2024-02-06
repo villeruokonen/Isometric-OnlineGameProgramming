fed19c5a0953caa892d5e5876657092724722bf2
Ville Ruokonen <ruokonenv@gmail.com>
2024-02-06 10:00:06 +0200

    Implemented Relay connection

--- Assets/Scripts/NetworkController.cs
+++ Assets/Scripts/NetworkController.cs
@@ -2,40 +2,89 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using Unity.Services;
 using UnityEngine.UI;
+using System.Threading.Tasks;
+using Unity.Services.Core;
+using Unity.Services.Authentication;
+using Unity.Services.Relay.Models;
+using Unity.Services.Relay;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
 
 public class NetworkController : NetworkBehaviour
 {
     [SerializeField] private Button hostButton;
     [SerializeField] private Button joinButton;
     [SerializeField] private GameObject connectionPanel;
+    [SerializeField] private TMPro.TMP_Text _joincodeDisplay;
+    [SerializeField] private TMPro.TMP_InputField _joinCodeInput;
+
     private void Awake()
     {
         hostButton.onClick.AddListener(Host);
         joinButton.onClick.AddListener(Join);
     }
 
-    void Start()
+    async void Host()
     {
-        
+        string joinCode = await StartRelayHost();
+
+        if(NetworkManager.Singleton.IsHost)
+        {
+            connectionPanel.SetActive(false);
+            _joincodeDisplay.text = "Join code: " + joinCode;
+        }
+
+        else Debug.LogError("Failed to start host");
+    }
+    async void Join()
+    {
+        bool succesful = await JoinWithRelayAsync(_joinCodeInput.text);
+
+        if(succesful)
+        {
+            connectionPanel.SetActive(false);
+        }
+        else Debug.LogError("Failed to join");
     }
 
-    
-    void Update()
+    // Returns the join code if successful, otherwise returns null
+    private async Task<string> StartRelayHost()
     {
-        
-    }
-    void Host()
-    {
-        NetworkManager.Singleton.StartHost();
-        connectionPanel.SetActive(false);
+        await UnityServices.InitializeAsync();
+
+        // Sign in if not signed in
+        if (!AuthenticationService.Instance.IsSignedIn)
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        }
+
+        Allocation alloc = await RelayService.Instance.CreateAllocationAsync(maxConnections: 2);
+
+        // We set UnityTransport's relay server data with the allocation and the protocol
+        var utp = NetworkManager.Singleton.GetComponent<UnityTransport>();
+        utp.SetRelayServerData(new RelayServerData(alloc, "dtls"));
+
+        var joinCode = await RelayService.Instance.GetJoinCodeAsync(alloc.AllocationId);
+
+        // We return the join code if we succesfully start hosting
+        return NetworkManager.Singleton.StartHost() ? joinCode : null;
     }
 
-    void Join()
+    private async Task<bool> JoinWithRelayAsync(string joinCode)
     {
-        NetworkManager.Singleton.StartClient();
-        connectionPanel.SetActive(false);
-    }
+        await UnityServices.InitializeAsync();
+        if(!AuthenticationService.Instance.IsSignedIn)
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        }
 
+        var alloc = await RelayService.Instance.JoinAllocationAsync(joinCode);
+        var utp = NetworkManager.Singleton.GetComponent<UnityTransport>();
+        utp.SetRelayServerData(new RelayServerData(alloc, "dtls"));
+
+        return !string.IsNullOrEmpty(joinCode) && NetworkManager.Singleton.StartClient();
+    }
 
 }
